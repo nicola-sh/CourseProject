9f82fb481527da43aaf036a109d2bd12076b080c
Nicola <shirnin.kolia@gmail.com>
2022-11-20 01:34:52 +0300

    go mod + first main

--- Hike/cmd/main.go
+++ Hike/cmd/main.go
@@ -1,5 +1,10 @@
-package main
+package hike
+
+import (
+	"net/http"
+)
 
 func main() {
 
+	http.ListenAndServe(":9090", nil)
 }
--- Hike/go.mod
+++ Hike/go.mod
@@ -1,3 +1,3 @@
-module example.com/m/v2
+module github.com/nicola-sh/CourseProject/tree/main/Hike/m/v2
 
 go 1.19
